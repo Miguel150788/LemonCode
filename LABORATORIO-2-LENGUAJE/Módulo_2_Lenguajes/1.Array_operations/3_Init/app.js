a019d8f521ad4bf46bdb077f27022266fe9acbef
Miguel Bueno <109579157+Miguel150788@users.noreply.github.com>
2022-11-03 15:25:54 +0100

    Delete LABORATORIO-2-LENGUAJE/Módulo_2_Lenguajes directory

--- LABORATORIO-2-LENGUAJE/Módulo_2_Lenguajes/1.Array_operations/3_Init/app.js
+++
@@ -1,9 +0,0 @@
-const init = (array) => {
-    const arrayNew = [...array];
-    arrayNew.pop();
-    return arrayNew;
-}
-
-const array = ["Hola", 3, 4, 5, 5];
-console.log("Array original",array);
-console.log("Retorno de array modificada new" ,init(array));
--- LABORATORIO-2-LENGUAJE/Módulo_2_Lenguajes/1.Array_operations/4_Last/app.js
+++
@@ -1,6 +0,0 @@
-
-const last = (array) => (array[array.length - 1 ]);
-
-const array = ["Hola", 3, 4, 5, 5, "Yo"];
-
-console.log(last(array));
